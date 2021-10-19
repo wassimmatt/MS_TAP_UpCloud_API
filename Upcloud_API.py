1436e7ea54a22b5a35d442c855568ad700635082
Maggie liu <liuke0712@126.com>
2021-10-19 13:59:57 -0400

    Add perf function

--- Upcloud_API.py
+++ Upcloud_API.py
@@ -22,6 +22,7 @@ class Upcloud_API:
         self.plan8 = "16xCPU-64GB"
         self.plan9 = "20xCPU-96GB"
         self.plan10 = "20xCPU-128G"
+        # self.logs
 
 
     # login user
@@ -39,7 +40,7 @@ class Upcloud_API:
                                 format=serialization.PrivateFormat.TraditionalOpenSSL,
                                 encryption_algorithm=serialization.NoEncryption())
         login_user = login_user_block(
-            username='test_user',
+            username='root',
             ssh_keys=[public_key],
             create_password=False
         )
@@ -48,49 +49,40 @@ class Upcloud_API:
         return login_user
 
 
-    # def get_zones(self):
-    #     zones = self.manager.get_zones()['zones']['zone']
-    #     # print(zones)
-    #     zone_list = []
-    #     for zone in zones:
-    #         zone_list.append(zone['id'])
-    #     return zone_list
+    def get_zones(self):
+        zones = self.manager.get_zones()['zones']['zone']
+        zone_list = []
+        for zone in zones:
+            zone_list.append(zone['id'])
+        return zone_list
 
 
     def get_templates(self):
         templates = self.manager.get_templates()
-        print(templates)
-        # # template_list=[]
-        # print(templates[0].keys())
-        # i = 0
-        # while i < len(templates):
-        #     if os == templates[i].keys():
-        #         return templates[i][os]
-        #         break
-        #     else:
-        #         continue
-        #     i += 1
+        return templates
 
 
     #new server creation
-    def create_server(self, plan, zone, hostname, os, os_size, sec_stg_size,sec_stg_tier,login_user):
+    def create_server(self, plan, zone, hostname, os, os_size, login_user):
         server = Server(
             plan=plan,
             hostname=hostname,
             zone=zone,  # All available zones with ids can be retrieved by using manager.get_zones()
             storage_devices=[
-                # OS: template storage UUID, all available os templates can be retrieved by calling manager.get_templates()
-                # Note: the storage os template uuid:s will change when OS is updated. So check that the UUID is correct
-                # default tier: maxIOPS, the 100k IOPS storage backend
                 Storage(os=os, size=os_size),
-                # secondary storage, hdd for reduced speed & cost
-                Storage(size=sec_stg_size, tier=sec_stg_tier)
             ],
             login_user=login_user  # user and ssh-keys
         )
         self.manager.create_server(server)
-        # server.add_tags([self.tag])
+        self.server_status(server)
         return server
+
+
+    #get current server status
+    def server_status(self,uuid):
+        server_status = self.manager.get_server(uuid).to_dict()['state']
+        server_name = self.manager.get_server(uuid).to_dict()['hostname']
+        return "Current status of server: "+server_name+" is "+server_status
 
 
     #get all server list
@@ -126,6 +118,31 @@ class Upcloud_API:
             raise e
 
 
+    # check the performance of linux server
+    def perform_statistic_linux(self,uuid):
+        try:
+            server = self.manager.get_server(uuid).to_dict()
+            ip_addr = server['ip_addresses']
+            perform_info = []
+            for ip in ip_addr:
+                if ip['access'] =='public' and ip['family'] == 'IPv4':
+                    ssh = paramiko.SSHClient()
+                    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                    ssh.connect(ip['address'],port=22,username='root',key_filename='.\private_key.pem')
+                    command = 'export TERM=xterm && top -n 1 -b'
+                    # command = 'export TERM=xterm && mpstat'
+                    stdin, stdout, stderr = ssh.exec_command(command)
+                    lines = stdout.readlines()
+                    err_lines = stderr.readlines()
+                    break
+            if err_lines:
+                return err_lines
+            else:
+                return lines
+        except Exception as e:
+            raise e
+
+
     #delete a vm based on the uuid
     def rm_server(self,uuid):
         server = self.manager.get_server(uuid)
@@ -136,72 +153,16 @@ class Upcloud_API:
         return "Selected server deleted."
 
 
-
 if __name__ == '__main__':
     ins = Upcloud_API()
-    # login_user=ins.logon_user()
     # login_user = ins.key_pair_login()
     # print(ins.server_list())
+    # print(ins.get_zones())
+    # print(ins.get_templates())
     # print(ins.single_server('00effc4b-47f5-4394-a357-0750c810b096'))
-    print(ins.access_console('00effc4b-47f5-4394-a357-0750c810b096'))
+    # print(ins.access_console('00effc4b-47f5-4394-a357-0750c810b096'))
     # print(ins.server_list())
-    # ins.get_templates()
-    # ins.create_server("2xCPU-4GB","uk-lon1","maggie.example.com", "01000000-0000-4000-8000-000020060100", "10", "100", "hdd", login_user)
-    # new_server=ins.server("2xCPU-4GB","uk-lon1","web1.example.com",login_user)
+    print(ins.server_status('00e3d773-a32e-4cea-9653-1df3543710fa'))
+    # print(ins.perform_statistic_linux('00e3d773-a32e-4cea-9653-1df3543710fa'))
+    # print(ins.create_server("2xCPU-4GB","uk-lon1","maggie.win.com", "01000000-0000-4000-8000-000030200200", "10",login_user))
     # ins.rm_server("0021e1da-be14-4440-8de6-f04b0650926b")
-
-#get server details
-# print(self.manager.get_server("0021e1da-be14-4440-8de6-f04b0650926b").to_dict())
-#get server states
-# print(manager.get_server("0021e1da-be14-4440-8de6-f04b0650926b").to_dict()['state'])
-#delete vms (stop first)
-
-#access the console of VM
-
-
-
-
-
-
-# @app.route('/')
-# class BaseAPI:
-#     api = "api.upcloud.com"
-#     api_version = "1.3"
-#     token = base64.b64encode("tapaug2021ee:gr4D334uG2021".encode())
-#
-#     def get(self, endpoint):
-#         conn = http.client.HTTPSConnection(self.api)
-#         url = "/" + self.api_version + endpoint
-#         headers = {
-#             "Authorization": "Basic " + self.token.decode(),
-#             "Content-Type": "application/json"
-#         }
-#         conn.request("GET", url, None, headers)
-#         res = conn.getresponse()
-#         self.printresponse(res.read())
-#
-#     def printresponse(self, res):
-#         data = res.decode(encoding="UTF-8")
-#         print(data)
-
-
-# class account(BaseAPI):
-#     endpoint = "/account"
-#
-#     def do(self):
-#         self.get(self.endpoint)
-#
-# class plans(BaseAPI):
-#     endpoint="/plan"
-#     def do(self):
-#         self.get(self.endpoint)
-# #
-# # class server(BaseAPI):
-# #     endpoint="/server"
-# #
-# #     def do(self):
-# #         self.get(self.endpoint)
-# #
-# if __name__ == '__main__':
-#     plans().do()
-#     # app.run(host='0.0.0.0')
