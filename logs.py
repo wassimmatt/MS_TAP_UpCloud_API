eb7f7ff7a6014909376007a8e5267fc9f90959ae
Maggie liu <liuke0712@126.com>
2021-10-20 10:59:09 -0400

    logs added

--- logs.py
+++ logs.py
@@ -1,28 +1,17 @@
 import logging
-import datetime
-import os
-import pathlib
 
-
-class vmlogs:
+class Logs:
     def __init__(self):
-        now = datetime.datetime.now()
-        self.logPath = os.getcwd()+'\logs\\'
-        self.logName = 'vm_logs.log'
-        self.logFile = self.logPath+self.logName
-        print(self.logFile)
-        logging.basicConfig(filename=self.logFile, encoding='utf-8', level=logging.INFO)
+        logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(message)s')
 
     def info_logger(self,content):
         logging.info(content)
 
-    def warning_logger(self,content):
+    def warning_logger(self, content):
         logging.warning(content)
 
-    def error_logger(self,content):
+    def error_logger(self, content):
         logging.error(content)
 
-    def debug_logger(self,content):
+    def debug_logger(self, content):
         logging.debug(content)
-
-ins = vmlogs()
